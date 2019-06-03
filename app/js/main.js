f296e6e9a2f6944cd1bfa6fd1c8493fdadbc6dce
hyundotio <hyunkseo@icloud.com>
2019-06-02 21:54:46 -0500

    tutorial selector ?? somehow gone but back
    tutorial selector ?? somehow gone but back

--- app/js/main.js
+++ app/js/main.js
@@ -811,6 +811,24 @@ $('.tab').bind('click', function(e) {
 		}
 	})
 })
+//Tutorial selector
+$('.tutorial-selectors').find('a').each(function(){
+    $(this).bind('click',function(e){
+        e.preventDefault();
+        let $this = $(this);
+        let $tutorialPages = $('.tutorial-pages');
+        let $tutorialPage = $tutorialPages.find('.'+$this.attr('data-tutorial'));
+        let $tutorialPageVideo = $tutorialPage.find('video');
+        if($tutorialPageVideo.length > 0){
+            $tutorialPageVideo[0].currentTime = 0;
+        }
+        $('.tutorial-selectors').find('.active').removeClass('active')
+        $this.addClass('active');
+        $tutorialPages.find('.active').removeClass('active');
+        $tutorialPage.addClass('active')
+    })
+})
+
 //Reset key generation form
 $('.key-generate-reset').bind('click', function(e) {
 	e.preventDefault();
