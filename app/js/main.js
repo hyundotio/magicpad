aa18fd8bfb6c203923b86ae6e06918a2c66c93f4
HYUN KYU SEO <hseo@us.ibm.com>
2019-06-06 22:27:21 -0500

    remove ios trial
    remove ios trial

--- app/js/main.js
+++ app/js/main.js
@@ -661,17 +661,6 @@ function uploadKey(type){
 //UI Bindings
 //UI Bindings
 //UI Bindings
-
-//ios scroll
-$('input').on('focus', function(e) {
-    e.preventDefault(); e.stopPropagation();
-    window.scrollTo(0,0); //the second 0 marks the Y scroll pos. Setting this to i.e. 100 will push the screen up by 100px.
-});
-$('textarea').on('focus', function(e) {
-    e.preventDefault(); e.stopPropagation();
-    window.scrollTo(0,0); //the second 0 marks the Y scroll pos. Setting this to i.e. 100 will push the screen up by 100px.
-});
-
 //mobile app Menu
 $('.mobile-menu').bind('click',function(){
 	let $mainNav = $('.main-nav');
