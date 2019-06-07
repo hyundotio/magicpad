c5083fe3ff721bd64062c7ec2ff39d664d6c707b
HYUN KYU SEO <hseo@us.ibm.com>
2019-06-06 21:36:33 -0500

    disable zoom on input touch
    disable zoom on input touch

--- app/js/main.js
+++ app/js/main.js
@@ -31,7 +31,7 @@ function init() {
       let viewheight = $(window).height();
       let viewwidth = $(window).width();
       let viewport = document.querySelector("meta[name=viewport]");
-      viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0, user-scalable=no");
+      viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0, user-scalable=0, maximum-scale=1");
   }, 300);
 }
 init();
