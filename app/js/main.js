29e1383b3089aabee03894f52416dbb8a589c23c
HYUN KYU SEO <hseo@us.ibm.com>
2019-06-10 02:32:30 -0500

    Converts PNG to JPG without transparency. Fixed HTML issues
    Converts PNG to JPG without transparency. Fixed HTML issues

--- app/js/main.js
+++ app/js/main.js
@@ -333,6 +333,7 @@ function convertStegMsg($type){
 				if(retrievedMsg.length > 0){
 					$('.import-stg-msg-label').text('Reimport steganograph');
 					$('.text-read').val(retrievedMsg).text(retrievedMsg);
+					readFormCheck();
 				} else {
 					$type.val('');
 					lipAlert('The imported steganograph does not contain a message.');
@@ -623,7 +624,32 @@ function encryptMessage(msg, signedToggle) {
 					let reader = new FileReader();
 					reader.onload = function(e){
 						let newImg = document.createElement('img');
+						let imgConvert = document.createElement('img');
 						newImg.onload = function(){
+							let imgCanvas = document.createElement("canvas");
+							let imgContext = imgCanvas.getContext("2d");
+							imgContext.canvas.width = newImg.width;
+							imgContext.canvas.height = newImg.height;
+							imgContext.fillStyle = '#FFFFFF';
+							imgContext.fillRect(0,0,newImg.width,newImg.height);
+							imgContext.drawImage(newImg, 0, 0, newImg.width, newImg.height);
+							let imgInfom = imgCanvas.toDataURL("image/jpeg", 1.0);
+							imgConvert.onload = function(){
+								if(parseInt(steg.getHidingCapacity(imgConvert)) >= session.lastEnc){
+									lipAlert('Selected steganograph host cannot store the encrypted message. Please try a larger image.');
+								} else {
+									createSteg(imgConvert,$('.steg-msg-download'),session.lastEnc);
+									$(imgCanvas).remove();
+									$(newImg).remove();
+									$(imgConvert).remove();
+									encryptStatus(signedToggle);
+									session.running = false;
+									$body.removeClass('loading');
+									viewEncMsg(true);
+								}
+							}
+							imgConvert.src = imgInfom;
+							/*
 							if(parseInt(steg.getHidingCapacity(newImg)) >= session.lastEnc){
 								lipAlert('Selected steganograph host cannot store the encrypted message. Please try a larger image.');
 							} else {
@@ -633,7 +659,7 @@ function encryptMessage(msg, signedToggle) {
 								session.running = false;
 								$body.removeClass('loading');
 								viewEncMsg(true);
-							}
+							}*/
 						}
 						newImg.src = e.target.result;
 					}
