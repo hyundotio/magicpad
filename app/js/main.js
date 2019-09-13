c9ced65309a41ba1c855b01e2f967bfd66ff115e
HYUN KYU SEO <hseo@us.ibm.com>
2019-09-13 15:38:21 -0500

    Created image optimization for steganography
    1. Minimum not required. Image will be resized with a random salt.
    2. Maximum is 1024 w/h with original ratio. Unless text requires a bigger canvas.

--- app/js/main.js
+++ app/js/main.js
@@ -564,27 +564,62 @@ const encryptMessage = function(msg, signedToggle) {
 					const stegSrc = await resolveLoadFileURL($stgHost);
 					const newImg = await resolveImg(stegSrc.result);
 					const imgCanvas = document.createElement("canvas");
-					let imgContext = imgCanvas.getContext("2d");
-					imgContext.canvas.width = newImg.width;
-					imgContext.canvas.height = newImg.height;
+					const imgContext = imgCanvas.getContext("2d");
+					const maxSize = 1024;
+					const imgSalt = Math.floor(Math.random() * (16 - 0 + 1)) + 0;
+					const pixelSpace = Math.ceil(Math.sqrt(((session.lastEnc.length * 16) / 3))) + imgSalt;
+					const imgRatio = Math.ceil(newImg.width) / Math.ceil(newImg.height);
+					const imgRatioInv = Math.ceil(newImg.height) / Math.ceil(newImg.width);
+					let imgWidth = newImg.width;
+					let imgHeight = newImg.height;
+
+					//determine final image size
+					if(maxSize < imgWidth || maxSize < imgHeight){
+						//Reduce image size if too big
+						let maxWidth = maxSize;
+						let maxHeight = maxSize;
+						if(imgRatio > 1){
+							//width is bigger;
+							maxHeight = maxHeight * imgRatioInv;
+						} else if (imgRatio < 1) {
+							//height is bigger
+							maxWidth = maxWidth * imgRatio;
+						}
+						imgWidth = maxWidth;
+						imgHeight = maxHeight;
+					}
+					if((pixelSpace**2) > (imgHeight * imgWidth)){
+						//Calculate minimum image size for text required and stretch;
+						//This will also determine if the max size is not big enough for the user given text.
+						let minWidth = pixelSpace;
+						let minHeight = pixelSpace;
+						if(imgRatio > 1){
+							//width is bigger;
+							minWidth = minWidth * imgRatio;
+						} else if (imgRatio < 1) {
+							//height is bigger
+							minHeight = minHeight * imgRatioInv;
+						}
+						imgWidth = minWidth;
+						imgHeight = minHeight;
+					}
+
+
+					imgContext.canvas.width = imgWidth;
+					imgContext.canvas.height = imgHeight;
 					imgContext.fillStyle = '#FFFFFF';
-					imgContext.fillRect(0,0,newImg.width,newImg.height);
-					imgContext.drawImage(newImg, 0, 0, newImg.width, newImg.height);
+					imgContext.fillRect(0,0,imgWidth,imgHeight);
+					imgContext.drawImage(newImg, 0, 0, imgWidth, imgHeight);
 					const imgInfom = imgCanvas.toDataURL("image/jpeg", 1.0);
 					const imgConvert = await resolveImg(imgInfom);
-					if(parseInt(steg.getHidingCapacity(imgConvert)) >= session.lastEnc){
-						$stgHost.val('');
-						throw (errorFinder('steglen'));
-					} else {
-						createSteg(imgConvert,$('.steg-msg-download'),session.lastEnc);
-						$(imgCanvas).remove();
-						$(newImg).remove();
-						$(imgConvert).remove();
-						encryptStatus(signedToggle);
-						session.running = false;
-						$body.removeClass('loading');
-						viewEncMsg(true);
-					}
+					createSteg(imgConvert,$('.steg-msg-download'),session.lastEnc);
+					$(imgCanvas).remove();
+					$(newImg).remove();
+					$(imgConvert).remove();
+					encryptStatus(signedToggle);
+					session.running = false;
+					$body.removeClass('loading');
+					viewEncMsg(true);
 				} else {
 					encryptStatus(signedToggle);
 					session.running = false;
