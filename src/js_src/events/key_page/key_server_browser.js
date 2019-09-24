79168f6e2b16f2e1311af4ea3b603997e074b6db
HYUN KYU SEO <hseo@us.ibm.com>
2019-09-24 17:52:08 -0500

    Popup for key generation now preserves state

--- src/js_src/events/key_page/key_server_browser.js
+++ src/js_src/events/key_page/key_server_browser.js
@@ -3,10 +3,13 @@ $('.open-keybrowser').bind('click',function(){
 	let $keyServerBrowserWindow = $('.key-server-browser-window');
 	let $popupTabContent = $keyServerBrowserWindow.find('.popup-tab-content');
 	let $popupTab = $keyServerBrowserWindow.find('.popup-tabs');
-	$popupTab.find('.active').removeClass('active');
-	$popupTab.find('.popup-tab').eq(0).addClass('active');
-	let tabOpen = $popupTab.find('.active').attr('data-tab');
-	$popupTabContent.find('.popup-tab-page.active').removeClass('active');
-	$popupTabContent.find('.'+tabOpen).addClass('active');
+	let $activePopupTab = $popupTabContent.find('.active');
+	if($activePopupTab.length == 0){
+		$popupTab.find('.active').removeClass('active');
+		$popupTab.find('.popup-tab').eq(0).addClass('active');
+		let tabOpen = $popupTab.find('.active').attr('data-tab');
+		$popupTabContent.find('.popup-tab-page.active').removeClass('active');
+		$popupTabContent.find('.'+tabOpen).addClass('active');
+	}
 	openPopup('.key-server-browser-window');
 })
